cda18aadaa0d69c1579e12546f073f3f69db35fd
FranciscoVieir <franciscovieira6397@gmail.com>
2023-01-23 18:31:57 -0300

    feat: ending to the Character class

---
+++ src/Character.ts
@@ -0,0 +1,88 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter, { SimpleFighter } from './Fighter';
+import Race, { Elf } from './Races';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._race = new Elf(name, Math.floor(Math.random() * 10) + 1);
+    this._dexterity = this._race.dexterity;
+    this._archetype = new Mage(name);
+    this._maxLifePoints = (this._race.maxLifePoints / 2);
+    this._lifePoints = this._maxLifePoints;
+    this._strength = Math.floor(Math.random() * 10) + 1;
+    this._defense = Math.floor(Math.random() * 10) + 1;
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: Math.floor(Math.random() * 10) + 1,
+    };
+  }
+
+  public get race(): Race {
+    return this._race;
+  }
+
+  public get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  public get defense(): number {
+    return this._defense;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public get energy(): Energy {
+    return {
+      type_: this._energy.type_,
+      amount: this._energy.amount,
+    };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._defense;
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._defense += Math.floor(Math.random() * 10) + 1;
+    this._strength += Math.floor(Math.random() * 10) + 1;
+    this._dexterity += Math.floor(Math.random() * 10) + 1;
+    this._maxLifePoints += Math.floor(Math.random() * 10) + 1;
+
+    if (this._maxLifePoints >= this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._energy.amount = 10;
+    this._lifePoints = this._maxLifePoints;
+  }
+}
